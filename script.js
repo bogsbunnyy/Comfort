798908d80a7ed510c74dad620346bf45793f0665
bogsbunnyy <142479430+bogsbunnyy@users.noreply.github.com>
2024-04-22 19:50:21 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
 const sentences = [
     
-    "Your presence fills each moment with warmth and light.",
-    "In a world full of wonders, you're one of the brightest.",
-    "Life blossoms with every smile you share.",
-    "Your existence adds color to the canvas of our lives.",
-    "With you, every day feels like a gift.",
-    "Your laughter is the melody that makes life's symphony complete.",
-    "The world is a better place because you're in it.",
-    "Your kindness radiates like sunshine on a cloudy day.",
-    "You are cherished more than words could ever express.",
-    "In a million ways, you make the world a better, brighter place, just by being you.",
-    "I'm proud of you",
+    "I labyuuu beri berii mats lodicakes q",
+    "I miss u beri berii matss",
+    "Na mimiss na kitaa sobra sobra",
+    "Na aappreciate kita lodicakes, sobra sobra",
+    "Salamat sa lahat ng ginagawa mo",
+    "Thank you for existing luvs",
+    "Love kita lods",
     ];
     
     function flashSentence() {
